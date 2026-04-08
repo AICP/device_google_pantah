227871773a7290fbe95e6c476192f927961b55d8
mosimchah <mosimchah@gmail.com>
2026-04-08 12:12:41 -0400

    panther: use PixelParts
    Change-Id: Id2264a197889334941fad43822acec4046012a3d

--- aicp_cheetah.mk
+++ aicp_cheetah.mk
@@ -34,3 +34,14 @@ $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DEVICE_MAINTAINERS="Duhjoker"
+
+# PixelParts app
+PRODUCT_PACKAGES += \
+    PixelParts
+
+# PixelParts init rc
+PRODUCT_PACKAGES += \
+    init.pixelparts.rc
+
+# PixelParts sepolicy
+BOARD_SEPOLICY_DIRS += packages/apps/PixelParts/sepolicy
